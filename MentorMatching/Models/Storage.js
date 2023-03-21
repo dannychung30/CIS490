4d017ba06dc15988823375054c30a8da69ec061d
Edgar Del Valle <delva009@csusm.edu>
2023-03-20 18:14:48 -0700

    Code refactoring

---
+++ MentorMatching/Models/Storage.js
@@ -0,0 +1,73 @@
+class Storage {
+  constructor(dbName) {
+    this.dbName = dbName;
+    this.initializeDatabase();
+  }
+
+  getAll() {
+    let db = JSON.parse(localStorage.getItem(this.dbName));
+    return db;
+  }
+
+  initializeDatabase() {
+    if (!localStorage.getItem(this.dbName)) {
+      localStorage.setItem(this.dbName, JSON.stringify({ data: [] }));
+    }
+  }
+
+  insertMany(data) {
+    localStorage.setItem(this.dbName, JSON.stringify(data));
+  }
+
+  insert(data) {
+    let db = JSON.parse(localStorage.getItem(this.dbName));
+    db.data.push(data);
+    localStorage.setItem(this.dbName, JSON.stringify(db));
+  }
+
+  find(query) {
+    let db = JSON.parse(localStorage.getItem(this.dbName));
+    let result = db.data.filter((doc) => {
+      for (let key in query) {
+        if (doc[key] !== query[key]) {
+          return false;
+        }
+      }
+      return true;
+    });
+
+    return result;
+  }
+
+  update(query, data) {
+    let db = JSON.parse(localStorage.getItem(this.dbName));
+    db.data.forEach((doc) => {
+      let match = true;
+      for (let key in query) {
+        if (doc[key] !== query[key]) {
+          match = false;
+          break;
+        }
+      }
+      if (match) {
+        for (let key in data) {
+          doc[key] = data[key];
+        }
+      }
+    });
+    localStorage.setItem(this.dbName, JSON.stringify(db));
+  }
+
+  remove(query) {
+    let db = JSON.parse(localStorage.getItem(this.dbName));
+    db.data = db.data.filter((doc) => {
+      for (let key in query) {
+        if (doc[key] !== query[key]) {
+          return true;
+        }
+      }
+      return false;
+    });
+    localStorage.setItem(this.dbName, JSON.stringify(db));
+  }
+}
--- MentorMatching/scripts/index.js
+++ MentorMatching/scripts/index.js
@@ -1,47 +1,42 @@
-const db = new Database();
 const myForm = document.getElementById('myForm');
 const menteeFile = document.getElementById('menteeFile');
 const mentorFile = document.getElementById('mentorFile');
 
-const mentees = [];
-const mentors = [];
+const keys = new Keys();
+
+const MenteeSurvey = new Storage(Keys.Mentee_Survey);
+const Mentees = new Storage(Keys.Mentees);
+
+const MentorSurvey = new Storage(Keys.Mentor_Survey);
+const Mentors = new Storage(Keys.Mentors);
 
 myForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  parseCSV(menteeFile.files[0], db.saveMenteeSurvey, db.saveMentees);
-
-  Papa.parse(menteeFile.files[0], {
-    complete: function (results) {
-      db.saveMenteeSurvey(results.data[0]);
-
-      for (let i = 1; i < results.data[0].length; i++) {
-        const mentee = {};
-        for (let j = 0; j < results.data[0].length; j++) {
-          mentee[results.data[0][j]] = results.data[i][j];
-        }
-        mentees[i - 1] = mentee;
-      }
-      db.saveMentees(mentees);
-    },
-  });
-
-  Papa.parse(mentorFile.files[0], {
-    complete: function (results) {
-      db.saveMentorSurvey(results.data[0]);
-
-      for (let i = 1; i < results.data[0].length; i++) {
-        const mentor = {};
-        for (let j = 0; j < results.data[0].length; j++) {
-          const question = results.data[0][j];
-          const answer = results.data[i][j];
-          mentor[question] = answer;
-        }
-        mentors[i - 1] = mentor;
-      }
-      db.saveMentors(mentors);
-    },
-  });
-
+  parseCSV(menteeFile.files[0], MenteeSurvey, Mentees);
+  parseCSV(mentorFile.files[0], MentorSurvey, Mentors);
   window.location.href = './question-selection.html';
 });
+
+/**
+ *
+ * @param {any} file
+ * @param {Storage} survey
+ * @param {Storage} user
+ */
+function parseCSV(file, survey, user) {
+  const users = [];
+  Papa.parse(file, {
+    complete: function (results) {
+      survey.insertMany(results.data[0]);
+
+      for (let i = 1; i < results.data[0].length; i++) {
+        const user = {};
+        for (let j = 0; j < results.data[0].length; j++) {
+          user[results.data[0][j]] = results.data[i][j];
+        }
+        users[i - 1] = user;
+      }
+      user.insertMany(users);
+    },
+  });
+}
