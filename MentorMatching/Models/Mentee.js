39293912e2aadb3c707e33cb746f5da6de618b9e
Edgar Del Valle <delva009@csusm.edu>
2023-03-27 11:17:48 -0700

    Removed unused code

--- MentorMatching/Models/Mentee.js
+++ MentorMatching/Models/Mentee.js
@@ -1,4 +1,12 @@
 class Mentee {
+  /**
+   *
+   * @param {String} id
+   * @param {{}[]} responses
+   * @param {String} email
+   * @param {String} firstName
+   * @param {String} lastName
+   */
   constructor(id, responses, email, firstName, lastName) {
     this.id = id;
     this.responses = responses;
@@ -7,29 +15,5 @@ class Mentee {
     this.lastName = lastName;
     this.MAX_MATCHES = 3;
     this.possible_matches = [];
-  }
-
-  /**
-   *
-   * @param {Mentor} mentor Mentor to add to possible matches.
-   * @returns {Boolean}     True if the operation was successful, false if the operation was unsuccesful.
-   */
-  addPossibleMatch(mentor) {
-    if (!this.isMaxedOut()) {
-      this.possible_matches = [mentor, ...this.possible_matches];
-      return true;
-    }
-    return false;
-  }
-
-  /**
-   *
-   * @returns {Boolean} Whether the mentee has reached the maximum amount of matches.
-   */
-  isMaxedOut() {
-    if (this.possible_matches.length == this.maxedMatches) {
-      return true;
-    }
-    return false;
   }
 }
--- MentorMatching/Models/Mentor.js
+++ MentorMatching/Models/Mentor.js
@@ -2,6 +2,10 @@ class Mentor {
   /**
    *
    * @param {String} id
+   * @param {{}[]} responses
+   * @param {String} email
+   * @param {String} firstName
+   * @param {String} lastName
    */
   constructor(id, responses, email, firstName, lastName) {
     this.id = id;
@@ -11,28 +15,5 @@ class Mentor {
     this.lastName = lastName;
     this.mentees = [];
     this.max_mentees = 1;
-  }
-
-  /**
-   *
-   * @param {Mentee} new_mentee New mentee that should be added.
-   * @returns {Boolean}         Returns true if operation was successful, false otherwise.
-   */
-  addMentee(new_mentee) {
-    if (!this.isMaxedOut()) {
-      mentees = [new_mentee, ...this.mentees];
-    }
-    return false;
-  }
-
-  /**
-   *
-   * @returns {Boolean} Returns true if the mentor has reached the maximum amount of mentees has been reached, false otherwise.
-   */
-  isMaxedOut() {
-    if (this.mentees.length == this.max_mentees) {
-      return true;
-    }
-    return false;
   }
 }
