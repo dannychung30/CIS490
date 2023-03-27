aeea5ca260a72080163c86c173859d7748678c8d
Edgar Del Valle <delva009@csusm.edu>
2023-03-26 18:15:01 -0700

    Started working on adding to possble matches

--- MentorMatching/Models/Matcher.js
+++ MentorMatching/Models/Matcher.js
@@ -7,17 +7,23 @@ class Matcher {
    * @returns {none}
    */
   go_through_data() {
-    const mentees = new Storage(Keys.Mentees).getAll();
+    const mentees = new Storage(Keys.Mentees);
     const mentors = new Storage(Keys.Mentors).getAll();
 
-    mentees.forEach((mentee) => {
+    const mentees_copy = new Storage(Keys.Mentees).getAll();
+
+    mentees_copy.forEach((mentee) => {
       mentors.forEach((mentor) => {
         let total_score = this.tally_score(mentee, mentor);
-        console.log(
-          `Mentee: ${mentee.id} and Mentor: ${mentor.id} scored: ${total_score}`
-        );
+        // console.log(
+        //   `Mentee: ${mentee.id} and Mentor: ${mentor.id} scored: ${total_score}`
+        // );
+        mentee.possible_matches.push({ mentor: mentor.id, score: total_score });
       });
     });
+
+    mentees.clear();
+    mentees.insertMany(mentees_copy);
   }
 
   /**
@@ -51,9 +57,9 @@ class Matcher {
    */
   get_score(mentee_answer, mentor_answer) {
     let score = fuzzball.ratio(mentee_answer, mentor_answer);
-    console.log(
-      `Mentee Answered: ${mentee_answer}\nMentor Answered: ${mentor_answer}`
-    );
+    // console.log(
+    //   `Mentee Answered: ${mentee_answer}\nMentor Answered: ${mentor_answer}`
+    // );
     return score;
   }
 }
