0d37f7a28f735bd3975f1be314a4db20e3d75518
Edgar Del Valle <edqardv@gmail.com>
2023-03-31 16:23:46 -0700

    Fixed form on pairing page

--- MentorMatching/src/results/index.js
+++ MentorMatching/src/results/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import Results from './Results.js';
+import '../styles/global.css';
 
 const root = createRoot(document.getElementById('app'));
 root.render(<Results />);
