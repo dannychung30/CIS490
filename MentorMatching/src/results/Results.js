b142bca58c5540f7dc1eb6768b5cd0621026e49f
Edgar Del Valle <edqardv@gmail.com>
2023-03-31 18:52:23 -0700

    Adding test components

--- MentorMatching/src/results/Results.js
+++ MentorMatching/src/results/Results.js
@@ -1,10 +1,22 @@
 import AnotherComponent from './components/AnotherComponent';
 import Header from './components/Header';
+import Storage from '../Models/Storage';
+import Keys from '../Models/Keys';
+import MenteeCard from './components/MenteeCard';
 
 export default function Results() {
+  const mentees = new Storage(Keys.Mentees).getAll();
+  console.log(mentees);
+
   return (
     <div>
-      <Header />
+      {mentees.map((mentee) => (
+        <MenteeCard
+          key={mentee.id}
+          name='Test'
+          matches={mentee.possible_matches}
+        />
+      ))}
     </div>
   );
 }
