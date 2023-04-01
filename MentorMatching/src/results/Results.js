a73d3399279d6f4b085d4ca4edeb254e5b8aafa0
Danny Chung <dannychung6158@gmail.com>
2023-04-01 11:56:11 -0700

    From edgar: getting results for mentees

--- MentorMatching/src/results/Results.js
+++ MentorMatching/src/results/Results.js
@@ -1,10 +1,21 @@
 import AnotherComponent from './components/AnotherComponent';
 import Header from './components/Header';
+import Storage from '../Models/Storage';
+import Keys from '../Models/Keys';
+import MenteeCard from './components/MenteeMatchesCard';
 
 export default function Results() {
+  const mentees = JSON.parse(localStorage.getItem("Mentees"));
+
   return (
-    <div>
-      <Header />
+    <div className='results-list'>
+      {mentees.map((mentee) => (
+        <MenteeCard
+          key={mentee.id}
+          name='Mentee'
+          matches={mentee.possible_matches}
+        />
+      ))}
     </div>
   );
 }
