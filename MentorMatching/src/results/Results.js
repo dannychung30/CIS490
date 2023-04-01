440556aa998a7f068270b59ada44b1c07f938d97
Edgar Del Valle <edqardv@gmail.com>
2023-04-01 12:11:53 -0700

    Changed data passed into components

--- MentorMatching/src/results/Results.js
+++ MentorMatching/src/results/Results.js
@@ -1,19 +1,15 @@
-import AnotherComponent from './components/AnotherComponent';
-import Header from './components/Header';
-import Storage from '../Models/Storage';
-import Keys from '../Models/Keys';
-
 import MenteeCard from './components/MenteeMatchesCard';
 
 export default function Results() {
   const mentees = JSON.parse(localStorage.getItem('Mentees'));
+  console.log(mentees);
 
   return (
     <div className='results-list'>
       {mentees.map((mentee) => (
         <MenteeCard
           key={mentee.id}
-          name='Mentee'
+          name={`${mentee.firstName} ${mentee.lastName}`}
           matches={mentee.possible_matches}
         />
       ))}
--- MentorMatching/src/results/components/MenteeMatchesCard.jsx
+++ MentorMatching/src/results/components/MenteeMatchesCard.jsx
@@ -1,20 +1,18 @@
 export default function MenteeCard(props) {
-    return (
-        <div className="mentee-cards">
-            <div>
-                <h2 keys={props.name}>{props.name}</h2>
-            </div>
-            {
-                props.matches.map((match) => {
-                    return (
-                        <div>
-                            <p key={match.mentor}>
-                                {match.mentor} matched <strong>{match.score}%</strong>
-                            </p>
-                        </div>
-                    );
-                })
-            }
-        </div >
-    );
+  return (
+    <div className='mentee-cards'>
+      <div>
+        <h2 keys={props.name}>{props.name}</h2>
+      </div>
+      {props.matches.map((match) => {
+        return (
+          <div>
+            <p key={match.mentor.name}>
+              {match.mentor.firstName} matched <strong>{match.score}%</strong>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
